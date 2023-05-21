0a6cddeb38815ef811a616e7b80c617c2053d052
Elena Leon <91587602+elenal63@users.noreply.github.com>
2023-05-20 21:56:04 -0600

    Add files via upload

---
+++ TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Elena Leon on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            StartView()
+        }
+    }
+}
